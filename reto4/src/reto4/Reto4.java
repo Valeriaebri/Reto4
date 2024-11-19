dae8c1b41f3f3ab0abd83cfa52bfb6c4440c4ff8
Tron89 <claudiuacricota@gmail.com>
2024-11-19 11:54:14 +0100

    funciones

--- reto4/src/reto4/Reto4.java
+++ reto4/src/reto4/Reto4.java
@@ -25,9 +25,30 @@ public class Reto4 {
 	
 	// Parte de Claudiu
 
+	public static void calcularHije(int padre, int made) { // te imprime el calculo de la talla diana si es hijo sumandole + 6,5 cm por ser niño
+		edadHijr=23;
+		while(false) {
+			if(edadhija != 0) {
+				int alturaHija = calculo(padre, made);
+				System.out.println("La altura de la hija es: " alturaHija);
+			}
+			
+		}
+		
+	}
 	
+	public static calcularHija(int pare, int mdre) { // te imprime el calculo de la talla diana si es hija restandole - 6,5 cm si es niña
+		System.out.println("La edad de la hijo es: "+pare+-6.5);
+	}
 	
+	public static void calcularAmbos(int prre, int madre) { // te imprime el calculo de si es hijo y tambien si es hija llamando a las otras dos funciones
+		calcularHije
+		calcularHija(madre, prre);
+	}
 	
+	public static int calculo(int padre, int mdre) { // Esta calculadora utiliza la fórmula que los pediatras llaman “talla diana”: talla del padre + talla de la madre dividido entre 2 ( /).
+		padre+2/mdre
+	}
 	
 	
 	
