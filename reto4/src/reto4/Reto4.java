4f55fc6525d1681d2720dddc1aba6f9064908f45
Valeriaebri <vebriferr@gmail.com>
2024-11-19 12:17:31 +0100

    Update Reto4.java

--- reto4/src/reto4/Reto4.java
+++ reto4/src/reto4/Reto4.java
@@ -7,19 +7,13 @@ public class Reto4 {
 	public static void main(String[] args) {
 		// Parte de Bianca
 		
-		Scanner sc = new Scanner (System.out);
+		Scanner sc = new Scanner (System.in);
 		int numero = 0;
-
 		System.out.println("Introduce un número para calcular la estatura máxima de tu hij@: "); // se introduce aquí un número para seleccionar en el men
-		
 		System.out.println("/n1 Tengo hijo /n2 Tengo hija n3/ Tengo hijo e hija /n4 Salir."); 
 		numero = sc.nextInt(); // aqui se escanea el número introducido
-		
 		int opcion = funciones.dimeEntero("Eso no es un numero", sc); // en caso de error
 
-		if (opcion == 0){
-		break;
-		}
 		
 		int madre = 0;
 		int padre = 0;
@@ -47,16 +41,6 @@ public class Reto4 {
 		}
 		}
 		
-		
-		
-		// parte de Bianca
-		
-		
-		
-		
-		
-		
-		
 	}
 	
 	
@@ -65,32 +49,29 @@ public class Reto4 {
 	public static void calcularHije(int padre, int made) { // te imprime el calculo de la talla diana si es hijo sumandole + 6,5 cm por ser niño
 		edadHijr=23;
 		while(false) {
-			if(edadhija != 0) {
+			if(edadhijr != 0) {
 				int alturaHija = calculo(padre, made);
 				System.out.println("La altura de la hija es: " alturaHija);
-			}
-			
 		}
-		
+		}
 	}
 	
 	public static calcularHija(int pare, int mdre) { // te imprime el calculo de la talla diana si es hija restandole - 6,5 cm si es niña
-		System.out.println("La edad de la hijo es: "+pare+-6.5);
+		System.out.println("La edad de la hija es: "+(calculo(int padre, int mdre)-6.5));
 	}
-	
 	public static void calcularAmbos(int prre, int madre) { // te imprime el calculo de si es hijo y tambien si es hija llamando a las otras dos funciones
 		calcularHije
 		calcularHija(madre, prre);
 	}
 	
 	public static int calculo(int padre, int mdre) { // Esta calculadora utiliza la fórmula que los pediatras llaman “talla diana”: talla del padre + talla de la madre dividido entre 2 ( /).
-		padre+2/mdre
+		return (padre+mdre)/2;
 	}
 	
 	
 	
 	
-	// parte de Bianca
+
 	
 	
 	
